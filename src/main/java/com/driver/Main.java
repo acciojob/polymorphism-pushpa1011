9a8556d91de9f3fa0c3766a5ac72d10e09216f7e
pushpa <pushpakumari886@gmail.com>
2026-03-23 20:43:52 +0530

    Assignment completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,9 +7,11 @@ public class Main {
         }
         public int product(int x, int y, int z) {
             return x * y * z;
+
         }
         public double product(double x, double y) {
             return x * y;
+
 
         }
 
